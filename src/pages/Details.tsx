2bbbf8b11c86ac33e21591c2911b49bd28c33bd5
Igor Mircic <igor.mircic@live.com>
2021-11-19 22:06:37 +0100

    fix breakepoints on details page

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -17,60 +17,71 @@ export const Details = () => {
   const country = countries.find(c => c.id === countryId)
 
   return (
-    <Container sx={{ py: 6 }}>
-      <Button component={Link} to="/" startIcon={<ArrowBack />}>
-        Back
-      </Button>
-      {country && (
-        <Grid container pt={8} spacing={{ xs: 4, md: 8, lg: 16 }}>
-          <Grid item xs={12} md={6}>
-            <Box
-              component="img"
-              src={country.flag}
-              alt={`${country.name}'s flag`}
-              sx={{ maxWidth: 500, width: '100%', boxShadow: 1 }}
-            />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Grid container>
-              <Grid item xs={12} pb={2}>
-                <Typography variant="h4">{country.name}</Typography>
-              </Grid>
-              <Grid item xs={12}>
-                <Grid container spacing={{ xs: 4, lg: 16 }} pb={6}>
-                  <Grid item>
-                    <List>
-                      <ListItem>{`Native name: ${country.nativeName}`}</ListItem>
-                      <ListItem>{`Population: ${country.population}`}</ListItem>
-                      <ListItem>{`Region: ${country.region}`}</ListItem>
-                      <ListItem>{`Sub region: ${country.subregion}`}</ListItem>
-                      <ListItem>{`Capital: ${country.capital}`}</ListItem>
-                    </List>
-                  </Grid>
-                  <Grid item>
-                    <List>
-                      <ListItem>{`Top level domain: ${country.tld}`}</ListItem>
-                      <ListItem>{`Currencies: ${country.currencies}`}</ListItem>
-                      <ListItem>{`Languages: ${country.languages}`}</ListItem>
-                    </List>
-                  </Grid>
+    <Container>
+      <Box pt={6}>
+        <Button component={Link} to="/" startIcon={<ArrowBack />}>
+          Back
+        </Button>
+        {country && (
+          <Grid container pt={6} spacing={{ xs: 6 }}>
+            <Grid item xs={12} xl={6}>
+              <Box
+                component="img"
+                src={country.flag}
+                alt={`${country.name}'s flag`}
+                sx={{ maxWidth: 500, width: '100%', boxShadow: 1 }}
+              />
+            </Grid>
+            <Grid item xs={12} xl={6}>
+              <Grid container>
+                <Grid item xs={12} pb={2}>
+                  <Typography variant="h4">{country.name}</Typography>
                 </Grid>
-                <Grid item>
-                  <Grid container spacing={2}>
-                    {country.borders?.map(b => (
-                      <Grid item key={b.id}>
-                        <Button component={Link} to={`/${b.id}`}>
-                          {b.name}
-                        </Button>
-                      </Grid>
-                    ))}
+                <Grid item xs={12}>
+                  <Grid
+                    container
+                    spacing={{ xs: 2, md: 8, lg: 10, xl: 16 }}
+                    pb={5}
+                  >
+                    <Grid item>
+                      <List>
+                        <ListItem>{`Native name: ${country.nativeName}`}</ListItem>
+                        <ListItem>{`Population: ${country.population}`}</ListItem>
+                        <ListItem>{`Region: ${country.region}`}</ListItem>
+                        <ListItem>{`Sub region: ${country.subregion}`}</ListItem>
+                        <ListItem>{`Capital: ${country.capital}`}</ListItem>
+                      </List>
+                    </Grid>
+                    <Grid item>
+                      <List>
+                        <ListItem>{`Top level domain: ${country.tld}`}</ListItem>
+                        <ListItem>{`Currencies: ${country.currencies}`}</ListItem>
+                        <ListItem>{`Languages: ${country.languages}`}</ListItem>
+                      </List>
+                    </Grid>
                   </Grid>
+                  {country.borders && country.borders.length > 0 && (
+                    <Grid item>
+                      <Grid container spacing={2}>
+                        <Grid item>
+                          <Box sx={{ mt: 8 }}>Borders:</Box>
+                        </Grid>
+                        {country.borders?.map(b => (
+                          <Grid item key={b.id}>
+                            <Button component={Link} to={`/${b.id}`}>
+                              {b.name}
+                            </Button>
+                          </Grid>
+                        ))}
+                      </Grid>
+                    </Grid>
+                  )}
                 </Grid>
               </Grid>
             </Grid>
           </Grid>
-        </Grid>
-      )}
+        )}
+      </Box>
     </Container>
   )
 }
